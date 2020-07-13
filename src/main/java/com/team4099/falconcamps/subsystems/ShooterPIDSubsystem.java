ad4e275d5bcbdc675169911c9661d301f933437a
S3b4azzz <mass8028@chsphantoms.org>
2020-07-13 18:28:01 +0000

    changes

--- src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
@@ -2,6 +2,7 @@ package com.team4099.falconcamps.subsystems;
 
 import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Config.PIDController;
 
 public class ShooterPIDSubsystem extends SubsystemBase {
     private final CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -9,12 +10,16 @@ public class ShooterPIDSubsystem extends SubsystemBase {
     private final CANEncoder shooterEncoder = shooterLeader.getEncoder();
 
    public ShooterPIDSubsystem() {
-            double ShooterP = 1.7/ 300
-            double ShooterI = 0.0;
-            double ShooterD = 5.5 * 14000;
-            super(new PIDController(ShooterP, ShooterI, ShooterD));
+            double shooterP = 1.7/ 300
+            double shooterI = 0.0;
+            double shooterD = 5.5 * 14000;
+            super(new PIDController(shooterP, shooterI, shooterD));
         }
-
-    @Override
+public double useMeasurement() {
+    return GetCurrentVelocity
+}
+public void useOutput(double setPoint, double output);
+  
+@Override
     public void periodic() {}
 }
