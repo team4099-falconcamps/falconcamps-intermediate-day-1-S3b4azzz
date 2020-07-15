a4c395ef19d5e70a0d8d521f607bd5cb89752828
S3b4azzz <mass8028@chsphantoms.org>
2020-07-15 18:14:01 +0000

    robot  container

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,14 +1,16 @@
 package com.team4099.falconcamps;
 
+import com.team4099.falconcamps.commands.shooter.ShootPIDSubsystemCommand;
 import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
 
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     private final ShooterPIDSubsystem shooter = new ShooterPIDSubsystem();
     XboxController driverController = new XboxController(0);
 
     public RobotContainer() {
-         new Trigger(driverController::getAButton).whileActive(new MyCommand(myMotor));
+        new Trigger(driverController::getAButton).whileActiveContinuous(new ShootPIDSubsystemCommand(shooter));
     }
 }
